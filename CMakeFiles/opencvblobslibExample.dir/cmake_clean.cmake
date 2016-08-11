1f238a3cf155f70a759f28d81a9596e078f0e9ed
aounleonardo <leonardo.aoun@epfl.ch>
2016-08-11 11:34:09 +0200

    added the cmake and make

---
+++ CMakeFiles/opencvblobslibExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/opencvblobslibExample.dir/main.cpp.o"
+  "opencvblobslibExample.pdb"
+  "opencvblobslibExample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/opencvblobslibExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
